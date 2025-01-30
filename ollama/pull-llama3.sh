240ce3a9fe22e45c406657267bfe303b8fbd4915
buithehai94 <buithehai329@gmail.com>
2025-01-30 11:22:31 +1100

    Update pull-llama3.sh

--- ollama/pull-llama3.sh
+++ ollama/pull-llama3.sh
@@ -1,1 +1,13 @@
-d
+#!/bin/bash
+
+# Start Ollama server in the background
+ollama serve &
+
+# Wait for Ollama server to start
+sleep 5
+
+# Pull the llama3.1 model
+ollama pull llama3.1
+
+# Wait for Ollama server to finish
+wait $!
