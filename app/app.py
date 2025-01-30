9067c3212e4750c76bf39ac98c0f70a83a37309b
buithehai94 <buithehai329@gmail.com>
2025-01-30 11:22:46 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,19 @@
-x
+import requests
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {"Chat": "Bot"}
+
+@app.get('/ask')
+def ask(prompt: str):
+    # Send a POST request to the Ollama API for generating a response
+    res = requests.post('http://ollama:11434/api/generate', json={
+        "prompt": prompt,
+        "stream": False,
+        "model": "llama3"
+    })
+
+    return Response(content=res.text, media_type="application/json")
